f336a367f7291292e193119c3d6346b5285101db
lais-dev <aluno@udicentro.local>
2024-03-07 22:16:59 -0300

    Projeto 2

---
+++ trabalho2/src/main/java/com/example/trabalho2/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.trabalho2;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
+    
+
